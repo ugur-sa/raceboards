4b4614cc9329d2f4c363cf88e5515c920a6ec3be
ugur-sa <sadiklarugur@gmail.com>
2023-02-13 23:21:00 +0100

    Add times route

---
+++ src/pages/api/times.ts
@@ -0,0 +1,16 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  res.status(200).json({ data: 'Hello World' });
+}
