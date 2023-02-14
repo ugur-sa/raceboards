0464121a2df91d3b13495ebda55543d968550c8c
ugur-sa <sadiklarugur@gmail.com>
2023-02-14 18:38:34 +0100

    sc

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,18 @@
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react';
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to including trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
+};
+
 export default function Login() {
   const supabase = useSupabaseClient();
 
@@ -13,7 +25,7 @@ export default function Login() {
           theme="dark"
           supabaseClient={supabase}
           socialLayout="horizontal"
-          redirectTo="raceboards.vercel.app"
+          redirectTo={getURL()}
         />
       </div>
     </div>
