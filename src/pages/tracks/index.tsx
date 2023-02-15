1803b06e01c2863549c023a0aed44ab1b0133894
ugur-sa <sadiklarugur@gmail.com>
2023-02-15 15:24:55 +0100

    Add tracks route for tracks table

---
+++ src/pages/tracks/index.tsx
@@ -0,0 +1,35 @@
+import { useSession } from '@supabase/auth-helpers-react';
+import { Time, Track } from 'types';
+import useSWR from 'swr';
+import Times from '@/components/Times';
+import Link from 'next/link';
+import Tracks from '@/components/Tracks';
+import Head from 'next/head';
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default function TracksPage() {
+  const session = useSession();
+  const { data: userTimes, error: timesError } = useSWR<Time[]>(
+    `/api/times/?id=${session?.user.id}`,
+    fetcher
+  );
+
+  const { data: tracks, error: tracksError } = useSWR<Track[]>(
+    '/api/track',
+    fetcher
+  );
+
+  if (timesError || tracksError) return <div>failed to load</div>;
+  if (!userTimes || !tracks) return <div>loading...</div>;
+
+  return (
+    <>
+      <Head>
+        <title>Tracks</title>
+      </Head>
+      <Link href={'/'}>Back to homepage</Link>
+      <Tracks tracks={tracks} />
+    </>
+  );
+}
