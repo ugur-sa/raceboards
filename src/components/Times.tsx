76fe75cf4d6a8cb87075d3d31d591a2008ba572f
ugur-sa <sadiklarugur@gmail.com>
2023-02-15 02:26:39 +0100

    Table for times

--- src/components/Times.tsx
+++ src/components/Times.tsx
@@ -7,32 +7,40 @@ export default function Times({
   times: Time[];
   tracks: Track[];
 }) {
-  //show all times for the user
-
+  //show the times for the user in a table with the track name and country as columns and the time as the row
   return (
     <>
-      <div className="">
-        {times?.map((time) => (
-          <div key={time.id}>
-            {/* List all times for the track */}
-            <div className="p-4">
-              <ul>
+      <h1 className="text-3xl">Times</h1>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th className="px-4 py-2">Track</th>
+            <th className="px-4 py-2">Country</th>
+            <th className="px-4 py-2">Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {times?.map((time) => (
+            <tr key={time.id}>
+              <td className="border px-4 py-2">
                 {tracks?.map((track) => (
-                  <li key={track.id}>
-                    {track.id == time.track_id ? (
-                      <p>
-                        {track.name} : {time.time}
-                      </p>
-                    ) : (
-                      ''
-                    )}
-                  </li>
+                  <div key={track.id}>
+                    {track.id == time.track_id ? track.name : ''}
+                  </div>
                 ))}
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
+              </td>
+              <td className="border px-4 py-2">
+                {tracks?.map((track) => (
+                  <div key={track.id}>
+                    {track.id == time.track_id ? track.country : ''}
+                  </div>
+                ))}
+              </td>
+              <td className="border px-4 py-2">{time.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
