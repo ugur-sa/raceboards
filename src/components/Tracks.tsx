f7189b82b584d4e085318d284c0b7f74337bc051
ugur-sa <sadiklarugur@gmail.com>
2023-02-15 15:24:02 +0100

    Create track table

--- src/components/Tracks.tsx
+++ src/components/Tracks.tsx
@@ -1,46 +1,34 @@
-import { Time, Track } from 'types';
+import { Track } from 'types';
 
-export default function Tracks({
-  tracks,
-  times,
-}: {
-  tracks: Track[];
-  times: Time[];
-}) {
+export default function Tracks({ tracks }: { tracks: Track[] }) {
+  //display all tracks in a table
   return (
-    <>
-      <h1 className="text-3xl">Tracks</h1>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {tracks?.map((track) => (
-          <div
-            key={track.id}
-            className="bg-white rounded-lg shadow-lg border border-black"
-          >
-            <div className="p-4">
-              <h2 className="text-xl font-semibold">{track.name}</h2>
-              <p className="text-gray-500">{track.country}</p>
-            </div>
-            {/* List all times for the track */}
-            <div className="p-4">
-              <h3 className="text-lg font-semibold">Times</h3>
-              <ul>
-                {times?.map((time) => (
-                  <li key={time.id}>
-                    {track.id == time.track_id ? (
-                      <p>
-                        {time.user_name} {time.time}
-                      </p>
-                    ) : (
-                      ''
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">Tracks</h1>
+        <div className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">ID</th>
+                <th className="px-4 py-2">Name</th>
+                <th className="px-4 py-2">Length</th>
+                <th className="px-4 py-2">Country</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tracks.map((track) => (
+                <tr key={track.id}>
+                  <td className="border px-4 py-2">{track.id}</td>
+                  <td className="border px-4 py-2">{track.name}</td>
+                  <td className="border px-4 py-2">{track.length}m</td>
+                  <td className="border px-4 py-2">{track.country}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </main>
+    </div>
   );
 }
