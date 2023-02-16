8300bc33a28c26ca396c9d76429ae9a2cccd3daa
ugur-sa <sadiklarugur@gmail.com>
2023-02-16 13:57:18 +0100

    Change to daisyUI table

--- src/components/Times.tsx
+++ src/components/Times.tsx
@@ -13,52 +13,50 @@ export default function Times({
   return (
     <>
       <div className="flex flex-col items-center justify-center py-2">
-        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-          <div className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-            <table className="table-auto">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2">Track</th>
-                  <th className="px-4 py-2">Country</th>
-                  <th className="px-4 py-2">Time</th>
-                  <th className="px-4 py-2">Delete</th>
+        <div className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <table className="table">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">Track</th>
+                <th className="px-4 py-2">Country</th>
+                <th className="px-4 py-2">Time</th>
+                <th className="px-4 py-2">Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {times?.map((time) => (
+                <tr key={time.id}>
+                  <td className="px-4 py-2">
+                    {tracks?.map((track) => (
+                      <div key={track.id}>
+                        {track.id == time.track_id ? track.name : ''}
+                      </div>
+                    ))}
+                  </td>
+                  <td className="px-4 py-2">
+                    {tracks?.map((track) => (
+                      <div key={track.id}>
+                        {track.id == time.track_id ? track.country : ''}
+                      </div>
+                    ))}
+                  </td>
+                  <td className="px-4 py-2">{time.time}</td>
+                  <td className="px-4 py-2">
+                    <button
+                      className="rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700"
+                      onClick={() => {
+                        deleteTime(time.id);
+                        mutate();
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
-              </thead>
-              <tbody>
-                {times?.map((time) => (
-                  <tr key={time.id}>
-                    <td className="border px-4 py-2">
-                      {tracks?.map((track) => (
-                        <div key={track.id}>
-                          {track.id == time.track_id ? track.name : ''}
-                        </div>
-                      ))}
-                    </td>
-                    <td className="border px-4 py-2">
-                      {tracks?.map((track) => (
-                        <div key={track.id}>
-                          {track.id == time.track_id ? track.country : ''}
-                        </div>
-                      ))}
-                    </td>
-                    <td className="border px-4 py-2">{time.time}</td>
-                    <td className="border px-4 py-2">
-                      <button
-                        className="rounded bg-red-500 py-2 px-4 font-bold text-white hover:bg-red-700"
-                        onClick={() => {
-                          deleteTime(time.id);
-                          mutate();
-                        }}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </main>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
