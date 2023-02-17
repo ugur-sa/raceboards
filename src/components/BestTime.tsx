56edaf705526b0bb39bb77acc2aa83d07ffcc21e
ugur-sa <sadiklarugur@gmail.com>
2023-02-17 17:48:39 +0100

    Add loading state

--- src/components/BestTime.tsx
+++ src/components/BestTime.tsx
@@ -15,39 +15,85 @@ export default function BestTimeTable() {
     fetcher
   );
 
+  const loadingArray = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22,
+  ];
+
+  const topThree = [1, 2, 3];
+
   if (timesError) return <div>failed to load</div>;
-  if (!bestTimes) return <Spinner />;
+  if (!bestTimes) return BestTimeLoading(loadingArray, topThree);
+  setTimeout(() => {
+    console.log('loading');
+  }, 3000);
 
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {bestTimes.map((bestTimeObject, index) => (
+    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+      {bestTimes.map((bestTimeObject) => (
         <div
           key={bestTimeObject.track.id}
-          className="rounded-lg border border-gray-600 bg-gray-800 p-4 shadow-lg"
+          className="grid grid-cols-12 place-items-center rounded-lg border border-gray-600 bg-gray-800 p-4 shadow-lg"
         >
-          <div className="flex items-center justify-between">
-            <h1 className="text-2xl font-bold text-white">
+          <p className=" text-3xl font-medium text-gray-300">
+            {bestTimeObject.track.season_order}
+          </p>
+          <div className="col-span-10 flex flex-col  items-start justify-center place-self-start pl-10">
+            <h1 className="text-xl font-bold text-white">
               {bestTimeObject.track.name}
             </h1>
+            {bestTimeObject.times.map((time, index) => (
+              <div key={time.id}>
+                <p className="text-sm text-white">
+                  {index === 0 && '🥇'}
+                  {index === 1 && '🥈'}
+                  {index === 2 && '🥉'}
+                  {time.time} {'-'} {time.username}
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="">
             <Image
               className="rounded-full"
               src={`/flags/${bestTimeObject.track.country.toLowerCase()}.png`}
               alt="flag"
-              width={30}
-              height={30}
+              width={100}
+              height={100}
             />
           </div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
-          {bestTimeObject.times.map((time, index) => (
-            <div key={time.id}>
-              <p className="text-white">
-                {index === 0 && '🥇'}
-                {index === 1 && '🥈'}
-                {index === 2 && '🥉'}
-                {time.time} {'-'} {time.username}
-              </p>
-            </div>
-          ))}
+function BestTimeLoading(loadingArray: number[], topThree: number[]) {
+  return (
+    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+      {loadingArray.map((loading) => (
+        <div
+          key={loading}
+          className="grid h-28 grid-cols-12 place-items-center rounded-lg border border-gray-600 bg-gray-800 p-4 shadow-lg"
+        >
+          <p className="animate-pulse text-3xl font-bold text-slate-400 opacity-20 blur-sm">
+            {loading}
+          </p>
+          <div className="col-span-10 flex flex-col  items-start justify-center place-self-start pl-10">
+            <h1 className="animate-pulse text-xl font-bold text-slate-400 opacity-20 blur-sm">
+              Track
+            </h1>
+            {topThree.map((top) => (
+              <div key={top}>
+                <p className="animate-pulse text-sm font-bold text-slate-400 opacity-20 blur-sm">
+                  1:30.000 - Username
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="">
+            <div className="h-12 w-12 animate-pulse rounded-full bg-slate-400 opacity-20 blur-sm"></div>
+          </div>
         </div>
       ))}
     </div>
