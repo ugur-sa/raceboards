bccb07ccd318aef7c00c8ea0db92e360f7eda70f
ugur-sa <sadiklarugur@gmail.com>
2023-02-17 23:01:13 +0100

    Check times from their time_in_ms values

--- src/pages/api/bestTimes.ts
+++ src/pages/api/bestTimes.ts
@@ -21,47 +21,11 @@ export default async function handler(
     //get users
     const users = await prisma.users.findMany();
 
-    //get the best time for each track but the times are saved as strings
-    //so we need to convert them to numbers but they are in this format "1:23.456"
-    //so we need to split the string and convert the numbers to milliseconds to check which is the best time and then convert it back to the string format
-    // const bestTimes = tracks.map((track) => {
-    //   const bestTime = times
-    //     .filter((time) => time.track_id === track.id)
-    //     .sort((a, b) => {
-    //       const aTime = a.time.split(':');
-    //       const bTime = b.time.split(':');
-
-    //       const aTimeInMs =
-    //         parseInt(aTime[0]) * 60 * 1000 + parseInt(aTime[1]) * 1000;
-    //       const bTimeInMs =
-    //         parseInt(bTime[0]) * 60 * 1000 + parseInt(bTime[1]) * 1000;
-
-    //       return aTimeInMs - bTimeInMs;
-    //     })[0];
-
-    //   return {
-    //     track_id: track.id,
-    //     time: bestTime?.time,
-    //     username: users.find((user) => user.id === bestTime?.user_id)?.name,
-    //   };
-    // });
-    // res.status(200).json(bestTimes);
-
     //get the top 3 best times for each track and order them by time
     const bestTimes = tracks.map((track) => {
       const bestTimes = times
         .filter((time) => time.track_id === track.id)
-        .sort((a, b) => {
-          const aTime = a.time.split(':');
-          const bTime = b.time.split(':');
-
-          const aTimeInMs =
-            parseInt(aTime[0]) * 60 * 1000 + parseInt(aTime[1]) * 1000;
-          const bTimeInMs =
-            parseInt(bTime[0]) * 60 * 1000 + parseInt(bTime[1]) * 1000;
-
-          return aTimeInMs - bTimeInMs;
-        })
+        .sort((a, b) => a.time_in_ms - b.time_in_ms)
         .slice(0, 3);
 
       return {
