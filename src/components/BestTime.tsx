2d85b086ebbfa846f9ce291284b4e3eddb865301
ugur-sa <sadiklarugur@gmail.com>
2023-02-18 21:34:26 +0100

    Move loading state to own component

--- src/components/BestTime.tsx
+++ src/components/BestTime.tsx
@@ -4,6 +4,7 @@ import { useSession } from '@supabase/auth-helpers-react';
 import Link from 'next/link';
 import Spinner from './Spinner';
 import Image from 'next/image';
+import BestTimeLoading from './BestTimeLoading';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -15,15 +16,8 @@ export default function BestTimeTable() {
     fetcher
   );
 
-  const loadingArray = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22,
-  ];
-
-  const topThree = [1, 2, 3];
-
   if (timesError) return <div>failed to load</div>;
-  if (!bestTimes) return BestTimeLoading(loadingArray, topThree);
+  if (!bestTimes) return <BestTimeLoading />;
 
   return (
     <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
@@ -71,38 +65,6 @@ export default function BestTimeTable() {
               width={100}
               height={100}
             />
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function BestTimeLoading(loadingArray: number[], topThree: number[]) {
-  return (
-    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-      {loadingArray.map((loading) => (
-        <div
-          key={loading}
-          className="grid h-28 grid-cols-12 place-items-center rounded-lg border border-gray-600 bg-gray-800 p-4 shadow-lg"
-        >
-          <p className="animate-pulse text-3xl font-bold text-slate-400 opacity-20 blur-sm">
-            {loading}
-          </p>
-          <div className="col-span-10 flex flex-col  items-start justify-center place-self-start pl-10">
-            <h1 className="animate-pulse text-xl font-bold text-slate-400 opacity-20 blur-sm">
-              Track
-            </h1>
-            {topThree.map((top) => (
-              <div key={top}>
-                <p className="animate-pulse text-sm font-bold text-slate-400 opacity-20 blur-sm">
-                  1:30.000 - Username
-                </p>
-              </div>
-            ))}
-          </div>
-          <div className="">
-            <div className="h-12 w-12 animate-pulse rounded-full bg-slate-400 opacity-20 blur-sm"></div>
           </div>
         </div>
       ))}
---
+++ src/components/BestTimeLoading.tsx
@@ -0,0 +1,37 @@
+export default function BestTimeLoading() {
+  const loadingArray = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22,
+  ];
+
+  const topThree = [1, 2, 3];
+  return (
+    <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+      {loadingArray.map((loading) => (
+        <div
+          key={loading}
+          className="grid h-28 grid-cols-12 place-items-center rounded-lg border border-gray-600 bg-gray-800 p-4 shadow-lg"
+        >
+          <p className="animate-pulse text-3xl font-bold text-slate-400 opacity-20 blur-sm">
+            {loading}
+          </p>
+          <div className="col-span-10 flex flex-col  items-start justify-center place-self-start pl-10">
+            <h1 className="animate-pulse text-xl font-bold text-slate-400 opacity-20 blur-sm">
+              Track
+            </h1>
+            {topThree.map((top) => (
+              <div key={top}>
+                <p className="animate-pulse text-sm font-bold text-slate-400 opacity-20 blur-sm">
+                  1:30.000 - Username
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="">
+            <div className="h-12 w-12 animate-pulse rounded-full bg-slate-400 opacity-20 blur-sm"></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
