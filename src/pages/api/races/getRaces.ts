3e33d60f415f6c984218c07f2bb3835db23d1db2
ugur-sa <sadiklarugur@gmail.com>
2023-02-23 21:49:38 +0100

    Create endpoint to get race data

---
+++ src/pages/api/races/getRaces.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    res.status(200).json({ message: 'Nothing to see here' });
+  }
+}
