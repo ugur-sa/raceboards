ccda3100708f7411d571c87a39f94424f4219864
ugur-sa <sadiklarugur@gmail.com>
2023-02-24 19:09:21 +0100

    Add utility function to convert ms to mm:ss.ms format

---
+++ utils/convertTime.ts
@@ -0,0 +1,16 @@
+const convertTime = (time: number) => {
+  const seconds = Math.floor(time / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const milliseconds = (time % 1000).toString().padStart(3, '0');
+  const secondsLeft = seconds % 60;
+
+  if (minutes > 0) {
+    return `${minutes}:${
+      secondsLeft < 10 ? '0' : ''
+    }${secondsLeft}.${milliseconds}`;
+  } else {
+    return `${secondsLeft}.${milliseconds}`;
+  }
+};
+
+export default convertTime;
