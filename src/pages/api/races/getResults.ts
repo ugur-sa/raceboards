451f212485cfafac04c12b6dc5f9e6945543b103
ugur-sa <sadiklarugur@gmail.com>
2023-02-28 13:54:50 +0100

    Creat getResults endpoint

---
+++ src/pages/api/races/getResults.ts
@@ -0,0 +1,184 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+import { Lap, Player, Race, Result, Session } from 'types';
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    //get query
+    const { id } = req.query;
+
+    const results = await prisma.results.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!results) {
+      res.status(404).json({ message: 'No results found' });
+      return;
+    }
+
+    const track_name: string = results.track_name!;
+    const result: Result = results.result as Result;
+
+    const practice: any = {};
+    const qualification: any = {};
+    const race: Race = {};
+
+    result.sessions.forEach((session: Session) => {
+      if (session.name === 'Practice') {
+      } else if (session.name === 'Qualification') {
+      } else if (session.name === 'Race') {
+        const unfixedLaps = session.laps;
+        if (session.laps.length !== session.lapsCount * result.players.length) {
+          session.laps = fixLaps(
+            session.laps,
+            session.lapsCount,
+            result.players
+          );
+        }
+        race.session = session.name;
+        race.winner = result.players[session.raceResult[0]].name;
+        race.best_lap = {
+          player:
+            result.players[
+              session.bestLaps.reduce((prev, curr) =>
+                prev.time < curr.time ? prev : curr
+              ).car
+            ].name,
+          time: session.bestLaps.reduce((prev, curr) =>
+            prev.time < curr.time ? prev : curr
+          ).time,
+        };
+        race.max_laps = session.lapsCount;
+        race.lasted_laps = Math.max(...session.lapstotal);
+        race.results = [];
+        session.raceResult.forEach((index) => {
+          race.results?.push({
+            player: result.players[index].name,
+            vehicle: result.players[index].car,
+            laps: session.lapstotal[index],
+            time: getTime(unfixedLaps, index, session.lapsCount),
+            best_lap: session.bestLaps[index].time,
+            led: getLedLapsAmount(
+              session.laps,
+              result.players,
+              session.lapsCount,
+              index
+            ),
+          });
+        });
+        //get the player with most laps led
+        race.most_laps_led = race.results.reduce((prev, curr) =>
+          prev.led > curr.led ? prev : curr
+        ).player;
+      }
+    });
+
+    const response = {
+      track_name: track_name,
+      result: [practice, qualification, race],
+    };
+
+    res.status(200).json(response);
+  }
+}
+
+function getTime(
+  laps: Lap[],
+  player: number,
+  lapsCount: number
+): { time: number; retired?: number } {
+  const playerLaps: Lap[] = [];
+
+  laps.forEach((lap) => {
+    if (lap.car === player) {
+      playerLaps.push(lap);
+    }
+  });
+
+  if (playerLaps.length === 0) return { time: 0 };
+
+  let time = 0;
+
+  playerLaps.forEach((lap) => {
+    if (lap.time === Infinity) return;
+    time += lap.time;
+  });
+
+  if (playerLaps.length != lapsCount) {
+    return { time: time, retired: lapsCount - playerLaps.length };
+  }
+
+  return { time: time };
+}
+
+function getLedLapsAmount(
+  laps: Lap[],
+  players: Player[],
+  lapsCount: number,
+  player: number
+) {
+  const playerObjects = players.map((player: Player, index) => {
+    return { index: index, led: 0, time: 0 };
+  });
+
+  let count = 0;
+
+  for (let i = 0; i < lapsCount; i++) {
+    for (let j = 0; j < laps.length; j++) {
+      if (laps[j].lap === i) {
+        playerObjects[laps[j].car].time += laps[j].time;
+        count++;
+        if (count >= players.length && count % players.length === 0) {
+          playerObjects.reduce((prev, curr) => {
+            return prev.time < curr.time ? prev : curr;
+          }).led++;
+        }
+      }
+    }
+  }
+  return playerObjects[player].led;
+}
+
+function fixLaps(laps: Lap[], lapsCount: number, players: Player[]): Lap[] {
+  if (laps.length === lapsCount * players.length) return laps;
+
+  const allPlayerLaps = players.map((player, index) => {
+    const playerLaps: Lap[] = [];
+
+    laps.forEach((lap) => {
+      if (lap.car === index) {
+        playerLaps.push(lap);
+      }
+    });
+
+    while (playerLaps.length < lapsCount) {
+      playerLaps.push({
+        lap: playerLaps.length,
+        car: index,
+        sectors: [0, 0, 0],
+        time: Infinity,
+        cuts: 0,
+        tyre: '',
+      });
+    }
+
+    return playerLaps;
+  });
+
+  let fixedLaps: Lap[] = [];
+
+  allPlayerLaps.forEach((playerLaps) => {
+    playerLaps.forEach((lap) => {
+      fixedLaps.push(lap);
+    });
+  });
+
+  return fixedLaps;
+}
