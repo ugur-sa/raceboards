4dc4f3d72ed50d360c155afa2efc63f3f9241621
ugur-sa <sadiklarugur@gmail.com>
2023-03-02 22:35:20 +0100

    Responsive Legend

--- src/components/Races/Legend.tsx
+++ src/components/Races/Legend.tsx
@@ -1,17 +1,17 @@
 const Legend = () => {
   return (
-    <div className="flex items-center justify-center gap-5 text-xs">
+    <div className="flex items-center justify-center gap-5 text-[8px] lg:text-xs">
       <div className="flex items-center justify-center gap-2">
         <p>Best personal lap time</p>
-        <div className="flex h-3 w-3 bg-[rgba(210,255,196,1)]"></div>
+        <div className="flex h-1 w-1 bg-[rgba(210,255,196,1)] lg:h-3 lg:w-3"></div>
       </div>
       <div className="flex items-center justify-center gap-2">
         <p>Best lap time / sector time</p>
-        <div className="h-3 w-3 bg-[rgba(255,206,206,1)]"></div>
+        <div className="h-1 w-1 bg-[rgba(255,206,206,1)] lg:h-3 lg:w-3"></div>
       </div>
       <div className="flex items-center justify-center gap-2">
         <p>Bad lap (107% of average lap time)</p>
-        <div className="h-3 w-3 bg-[rgba(238,207,161,1)]"></div>
+        <div className="h-1 w-1 bg-[rgba(238,207,161,1)] lg:h-3 lg:w-3"></div>
       </div>
     </div>
   );
