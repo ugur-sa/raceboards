eb713006f9ef42f81e3281a01c829c2d01986dd0
ugur-sa <sadiklarugur@gmail.com>
2023-03-02 22:35:34 +0100

    Add converTimeFull to utils

---
+++ utils/convertTimeFull.ts
@@ -0,0 +1,10 @@
+const convertTimeFull = (time: number) => {
+  const minutes = Math.floor(time / 60000);
+  const seconds = Math.floor((time % 60000) / 1000);
+  const milliseconds = time % 1000;
+  return `+${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}.${milliseconds.toString().padStart(3, '0')}`;
+};
+
+export default convertTimeFull;
