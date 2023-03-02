77f1aae6e337f32c4529d4a091b425cd8a6eda06
ugur-sa <sadiklarugur@gmail.com>
2023-03-03 00:07:21 +0100

    Make best sectors purple

--- src/components/Races/Legend.tsx
+++ src/components/Races/Legend.tsx
@@ -1,13 +1,17 @@
 const Legend = () => {
   return (
-    <div className="flex items-center justify-center gap-5 text-[8px] lg:text-xs">
+    <div className="hidden items-center justify-center gap-5 text-[8px] lg:flex lg:text-xs">
       <div className="flex items-center justify-center gap-2">
         <p>Best personal lap time</p>
         <div className="flex h-1 w-1 bg-green-400 lg:h-3 lg:w-3"></div>
       </div>
       <div className="flex items-center justify-center gap-2">
-        <p>Best lap time / sector time</p>
+        <p>Best lap time</p>
         <div className="h-1 w-1 bg-red-400 lg:h-3 lg:w-3"></div>
+      </div>
+      <div className="flex items-center justify-center gap-2">
+        <p>Best sector time</p>
+        <div className="h-1 w-1 bg-purple-700 lg:h-3 lg:w-3"></div>
       </div>
       <div className="flex items-center justify-center gap-2">
         <p>Bad lap (107% of average lap time)</p>
