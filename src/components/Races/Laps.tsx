292997454562b6539f3fa64464f96a82060f1b75
ugur-sa <sadiklarugur@gmail.com>
2023-03-02 22:52:15 +0100

    Make laps component dynamic for each session

--- src/components/Races/Laps.tsx
+++ src/components/Races/Laps.tsx
@@ -1,12 +1,9 @@
-import { useRouter } from 'next/router';
 import useSWR from 'swr';
 import { Lap, LapResponse } from 'types';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 const Laps = ({ race, session }: { race: string; session: string }) => {
-  const router = useRouter();
-
   const {
     data: lapsResponse,
     error: lapsError,
@@ -31,22 +28,25 @@ const Laps = ({ race, session }: { race: string; session: string }) => {
     }
   };
 
+  const response = lapsResponse[session as keyof LapResponse];
+
   return (
     <>
       {lapsResponse.Race ? (
         <div className="p-5">
-          {lapsResponse.Race.map((lapsObject, index) => (
+          {response.map((lapsObject, index) => (
             <div key={index} className="flex w-full flex-col gap-5 text-xl">
               <h2 className="text-3xl">{lapsObject.player}</h2>
               <table className="mb-5 w-full rounded-lg text-center">
                 <thead className="bg-gray-800 text-xs uppercase xl:text-xl">
                   <tr>
-                    <th>Lap</th>
+                    <th className="text-left">Lap</th>
                     <th>Time</th>
                     <th>Sector 1</th>
                     <th>Sector 2</th>
                     <th>Sector 3</th>
                     <th>Compound</th>
+                    <th>Cuts</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -70,33 +70,51 @@ const Laps = ({ race, session }: { race: string; session: string }) => {
                             ? 'bg-orange-300 hover:bg-orange-200 hover:text-black'
                             : ''
                         }
+                        ${
+                          lap.cuts > 0
+                            ? 'bg-[rgba(255,253,184,0.4)] hover:bg-[rgba(255,253,184,0.9)] hover:text-black'
+                            : ''
+                        }
                         `}
                         key={index}
                       >
-                        <td>{lap.lap + 1}</td>
-                        <td>{convertTime(lap.time)}</td>
+                        <td>
+                          {lap.cuts !== 0 ? (
+                            <div className="flex items-center gap-1">
+                              <p>{lap.lap + 1}</p>
+                              <p className="text-xs">(INVALID)</p>
+                            </div>
+                          ) : (
+                            <div className="flex items-center gap-1">
+                              <p>{lap.lap + 1}</p>
+                              <p className="text-xs"></p>
+                            </div>
+                          )}
+                        </td>
+                        <td>{lap.cuts > 0 ? '-' : convertTime(lap.time)}</td>
                         <td
                           className={`${
                             lap.best_sector_1 ? 'text-red-600' : ''
                           }`}
                         >
-                          {convertTime(lap.sectors[0])}
+                          {lap.cuts > 0 ? '-' : convertTime(lap.sectors[0])}
                         </td>
                         <td
                           className={`${
                             lap.best_sector_2 ? 'text-red-600' : ''
                           }`}
                         >
-                          {convertTime(lap.sectors[1])}
+                          {lap.cuts > 0 ? '-' : convertTime(lap.sectors[1])}
                         </td>
                         <td
                           className={`${
                             lap.best_sector_3 ? 'text-red-600' : ''
                           }`}
                         >
-                          {convertTime(lap.sectors[2])}
+                          {lap.cuts > 0 ? '-' : convertTime(lap.sectors[2])}
                         </td>
                         <td>{lap.tyre}</td>
+                        <td>{lap.cuts}</td>
                       </tr>
                     </>
                   ))}
