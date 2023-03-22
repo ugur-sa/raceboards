67388c22a77bfd669a7e74b7daa82a041535d98a
ugur-sa <sadiklarugur@gmail.com>
2023-03-22 15:54:30 +0100

    Change auth check component order

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,11 +12,9 @@ const Home = () => {
       <Head>
         <title>Raceboards</title>
       </Head>
-      {session?.user.aud !== 'authenticated' ? (
-        <Login />
-      ) : (
+      {session?.user.aud === 'authenticated' ? (
         <>
-          <div className="flex h-full flex-col bg-gray-800">
+          <div className="flex h-full flex-col bg-gradient-to-b from-slate-700 via-slate-800 to-slate-900">
             <Navbar />
             <main className="flex min-h-0 flex-grow flex-col items-center gap-10 p-10">
               <h1 className="place-self-center text-6xl font-bold text-white ">
@@ -26,6 +24,8 @@ const Home = () => {
             </main>
           </div>
         </>
+      ) : (
+        <Login />
       )}
     </>
   );
